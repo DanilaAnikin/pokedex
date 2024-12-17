f8ba288092ea95f14e0689c51a0a3e6520e529b8
Danila Anikin <danila.s.anikin@gmail.com>
2024-12-18 00:35:42 +0100

    .

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,36 +3,65 @@ import { getAllPokemons } from '~~/store/pokemon';
 import { type Pokemon } from '~~/types';
 
 const pokemons = ref<Pokemon[]>(await getAllPokemons());
+const searchInput = ref<string>("");
+const numberChecked = ref<boolean>(false);
+const nameChecked = ref<boolean>(true);
+
+const handleSearch = async() => {
+ const searchTerm = searchInput.value.toLowerCase();
+ let filteredPokemons;
+ pokemons.value = await getAllPokemons();
+
+ if (searchInput.value.length === 0) {
+  filteredPokemons = pokemons.value;
+ } else if(numberChecked.value) {
+   filteredPokemons = pokemons.value.filter((pokemon) => {
+    const pokemonId = pokemon.url.split("/")[6];
+    return pokemonId?.startsWith(searchTerm);
+   })
+ } else if(nameChecked.value) {
+   filteredPokemons = pokemons.value.filter((pokemon) => {
+    return pokemon.name.toLowerCase().startsWith(searchTerm);
+   })
+ } else {
+  filteredPokemons = pokemons.value;
+ }
+
+ pokemons.value = filteredPokemons;
+}
+
+// const redirect = (id: number) => window.location.pathname = id.toString();
+
 </script>
 
 <template>
-    <main class="main">
-      <header class="header-home">
-        <div class="container">
-          <div class="logo-wrapper bg-slate-500">
+    <main class="">
+      <header class="">
+        <div class="">
+          <div class="bg-slate-500">
             <img src="../img/pokeball.svg" alt="pokeball" />
             <h1>Pokedex</h1>
           </div>
-          <div class="search-wrapper">
-            <div class="search-wrap">
-              <img src="../img/search.svg" />
-              <input type="text" class="search-input body3-fonts" placeholder="Search" id="search-input" />
-              <img src="../img/cross.svg" alt="cross icon" class="search-close-icon" id="search-close-icon" />
+          <div class="">
+            <div class="">
+              <img src="../img/search.svg" @click="handleSearch()" />
+              <input v-model="searchInput" @keyup="handleSearch()" type="text" v-on:keyup.enter="handleSearch()" class="" placeholder="Search" id="search-input" />
+              <img src="../img/cross.svg" @click="searchInput=''; handleSearch()" alt="cross icon" class="" id="search-close-icon" />
             </div>
-            <div class="sort-wrapper">
-              <div class="sort-wrap">
-                <img src="../img/sorting.svg" alt="sorting icon" class="sort-icon" id="sort-icon" />
+            <div class="">
+              <div class="">
+                <img src="../img/sorting.svg" alt="sorting icon" class="" id="sort-icon" />
               </div>
-              <div class="filter-wrapper">
-                <p class="body2-fonts"> Sort by:</p>
-                <div class="filter-wrap">
+              <div class="">
+                <p class=""> Sort by:</p>
+                <div class="">
                   <div>
-                    <input type="radio" id="number" name="filters" value="number" checked>
-                    <label for="number" class="body3-fonts">Number</label>
+                    <input @click="numberChecked=true; nameChecked=false" type="radio" id="number" name="filters" value="number" checked>
+                    <label for="number" class="">Number</label>
                   </div>
                   <div>
-                    <input type="radio" id="name" name="filters" value="name" checked>
-                    <label for="number" class="body3-fonts">Name</label>
+                    <input @click="numberChecked=false; nameChecked=true" type="radio" id="name" name="filters" value="name" checked>
+                    <label for="number" class="">Name</label>
                   </div>
                 </div>
               </div>
@@ -40,27 +69,27 @@ const pokemons = ref<Pokemon[]>(await getAllPokemons());
           </div>
         </div>
       </header>
-      <section class="pokemon-list">
-        <div class="container">
-          <div class="list-wrapper">
+      <section class="">
+        <div class="">
+          <div class="">
 
           </div>
         </div>
-        <div class="not-found-message">
+        <div v-if="pokemons.length === 0" class="">
           Pokemon not found
         </div>
-        <div v-for="pokemon in pokemons" >
-          <div class="number-wrap">
-            <p class="caption-fonts">{{ pokemon.url.split("/")[6] }}</p>
+        <div v-if="pokemons.length != 0" v-for="pokemon in pokemons" @click="navigateTo(`./pokemon/${pokemon.url.split('/')[6]}`)" >
+          <div class="">
+            <p class="">{{ pokemon.url.split("/")[6] }}</p>
           </div>
-          <div class="img-wrap">
+          <div class="">
             <img
               :src="`https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemon.url.split('/')[6]}.svg`"
               :alt="pokemon.name"
             >
           </div>
-          <div class="name-wrap">
-            <p class="body3-fonts">{{ pokemon.name }}</p>
+          <div class="">
+            <p class="">{{ pokemon.name }}</p>
           </div>
         </div>
       </section>
---
+++ app/pages/pokemon/[id].vue
@@ -0,0 +1,107 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <main class="">
+        <header class="">
+            <div class="">
+                <div class="">
+                    <a href="./index.vue" class="">
+                        <img
+                            src="../../img/back-to-home.svg"
+                            alt="back to home"
+                            class=""
+                            id="back-btn"
+                        >
+                    </a>
+                    <div class="">
+                        <h1 class=""></h1>
+                    </div>
+                </div>
+                <div class="">
+                    <p class=""></p>
+                </div>
+            </div>
+        </header>
+        <div class="">
+            <a href="#" class="" id="left-arrow">
+                <img
+                    src="../../img/chevron_left.svg"
+                    alt="back"
+                >
+            </a>
+            <div class="">
+                <img src="" alt="">
+            </div>
+            <a href="#" class="">
+                <img
+                    src="../../img/chevron_right.svg"
+                    alt="forward"
+                >
+            </a>
+        </div>
+        <div class="">
+            <div class="">
+                <p class=""></p>
+                <p class=""></p>
+            </div>
+            <p class="">About</p>
+            <div class="">
+                <div class="">
+                    <div class="">
+                        <img
+                            src="../../img/weight.svg"
+                            alt="weight"
+                        >
+                        <p class=""></p>
+                    </div>
+                    <p class="">Weight</p>
+                </div>
+                <div class="">
+                    <div class="p">
+                        <img
+                            src="../../img/height.svg"
+                            alt="height"
+                        >
+                        <p class=""></p>
+                    </div>
+                    <p class="">Height</p>
+                </div>
+                <div class="">
+                    <div class=""></div>
+                    <p class="">Move</p>
+                </div>
+            </div>
+            <p class=""></p>
+            <p class="">Base Stats</p>
+            <div class="">
+                <div class="" data-stat="HP" >
+                    <p class="">HP</p>
+                    <p class=""></p>
+                    <progress value="" max="100" class=""></progress>
+                </div>
+                <div class="" data-stat="ATK" >
+                    <p class="">ATK</p>
+                    <p class=""></p>
+                    <progress value="" max="100" class=""></progress>
+                </div>
+                <div class="" data-stat="DEF" >
+                    <p class="">DEF</p>
+                    <p class=""></p>
+                    <progress value="" max="100" class=""></progress>
+                </div>
+                <div class="" data-stat="SATK" >
+                    <p class="">SATK</p>
+                    <p class=""></p>
+                    <progress value="" max="100" class=""></progress>
+                </div>
+            </div>
+        </div>
+        <img
+            src="../../img/pokedex.svg"
+            alt="pokedex"
+            class=""
+        >
+    </main>
+</template>
